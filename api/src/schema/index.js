125c7a0c02ddce599873b58bfc4089ef5e6d3d67
Daniel St. Clair <danielallanstclair@gmail.com>
2019-09-26 09:26:17 -0500

    Simplify example

--- api/src/schema/index.js
+++ api/src/schema/index.js
@@ -1,12 +1,27 @@
-const merge = require('lodash.merge');
 const { importSchema } = require('graphql-import');
 const depthLimit = require('graphql-depth-limit');
+// const merge = require('lodash.merge');
 
-const { resolvers: userResolvers } = require('./user');
-
+// const { resolvers: userResolvers } = require('./user');
+// const resolvers = merge({}, userResolvers);
 const schema = importSchema(`${__dirname}/schema.graphql`);
 
-const resolvers = merge({}, userResolvers);
+const users = [
+  { id: 1, name: 'St. Clair' },
+  { id: 2, name: 'Rico' },
+  { id: 3, name: 'Mateus' },
+  { id: 4, name: 'Franco' },
+  { id: 5, name: 'Daniela' },
+  { id: 6, name: 'Emmett' },
+  { id: 7, name: 'Dylan' },
+  { id: 8, name: 'Tom' },
+  { id: 9, name: 'Jacob' },
+];
+
+const getUsers = () => users;
+
+const Query = { getUsers };
+const resolvers = { Query };
 
 module.exports = {
   typeDefs: [schema],
