dc584c7fa92f13973b2ddde7f003dbc074a4c618
Daniel St. Clair <danielallanstclair@gmail.com>
2019-09-26 09:22:17 -0500

    Add cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const cors = require('cors');
 
 const schema = require('./schema');
 
@@ -7,6 +8,7 @@ const app = express();
 const server = new ApolloServer(schema);
 const port = 5000;
 server.applyMiddleware({ app });
+app.use(cors())
 
 app.listen({ port }, () =>
   console.log(`Server ready. go to localhost:${port}${server.graphqlPath}`)
